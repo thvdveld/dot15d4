8c8c953f4fa1cb7ff9fa556ed0fe34eed175e74c
Sam Clercky <sam.clercky@gmail.com>
2024-03-08 11:52:28 +0100

    Add extra note discussing why we have 128 bytes in the PacketBuffer

--- dot15d4/src/phy/driver.rs
+++ dot15d4/src/phy/driver.rs
@@ -26,7 +26,13 @@ pub trait Driver {
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 #[derive(Debug, Clone, PartialEq)]
 pub struct PacketBuffer {
-    /// The data of the frame that should be transmitted over the radio
+    /// The data of the frame that should be transmitted over the radio.
+    /// Normally, a MAC layer frame is 127 bytes long.
+    /// Some radios, like the one on the nRF52840, have the possibility to not having to include the checksum at the end, but require one extra byte to specify the length of the frame.
+    /// This results in this case in needing 126 bytes in total.
+    /// However, some radios like the one on the Zolertia Zoul, add extra information about the link quality.
+    /// In that case, the total buffer length for receiving data comes on 128 bytes.
+    /// If you would like to support a radio that needs more than 128 bytes, please file a PR.
     pub buffer: [u8; 128],
     /// Whether or not the buffer is ready to be read from
     pub dirty: bool,
