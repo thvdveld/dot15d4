6da4f490d24c0edf90e52d04b17570816875d408
Thibaut Vandervelden <thvdveld@vub.be>
2024-03-12 16:13:56 +0100

    fix logging

--- dot15d4/src/utils/log.rs
+++ dot15d4/src/utils/log.rs
@@ -3,49 +3,49 @@
 #[macro_export]
 macro_rules! error {
     ($($arg:tt)*) => {
-        #[cfg(feature = "log-defmt")]
+        #[cfg(feature = "defmt")]
         defmt::error!($($arg)*);
-        #[cfg(feature = "log-tracing")]
-        tracing::error!($($arg)*);
+        #[cfg(feature = "log")]
+        ::log::error!($($arg)*);
     };
 }
 
 #[macro_export]
 macro_rules! warn {
     ($($arg:tt)*) => {
-        #[cfg(feature = "log-defmt")]
+        #[cfg(feature = "defmt")]
         defmt::warn!($($arg)*);
-        #[cfg(feature = "log-tracing")]
-        tracing::warn!($($arg)*);
+        #[cfg(feature = "log")]
+        ::log::warn!($($arg)*);
     };
 }
 
 #[macro_export]
 macro_rules! info {
     ($($arg:tt)*) => {
-        #[cfg(feature = "log-defmt")]
-        defmt::info!($($arg)*);
-        #[cfg(feature = "log-tracing")]
-        tracing::info!($($arg)*);
+        #[cfg(feature = "defmt")]
+        ::defmt::info!($($arg)*);
+        #[cfg(feature = "log")]
+        ::log::info!($($arg)*);
     };
 }
 
 #[macro_export]
 macro_rules! debug {
     ($($arg:tt)*) => {
-        #[cfg(feature = "log-defmt")]
-        defmt::debug!($($arg)*);
-        #[cfg(feature = "log-tracing")]
-        tracing::debug!($($arg)*);
+        #[cfg(feature = "defmt")]
+        ::defmt::debug!($($arg)*);
+        #[cfg(feature = "log")]
+        ::log::debug!($($arg)*);
     };
 }
 
 #[macro_export]
 macro_rules! trace {
     ($($arg:tt)*) => {
-        #[cfg(feature = "log-defmt")]
-        defmt::trace!($($arg)*);
-        #[cfg(feature = "log-tracing")]
-        tracing::trace!($($arg)*);
+        #[cfg(feature = "defmt")]
+        ::defmt::trace!($($arg)*);
+        #[cfg(feature = "log")]
+        ::log::trace!($($arg)*);
     };
 }
